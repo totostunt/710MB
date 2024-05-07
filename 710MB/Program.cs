2cbc3f8e34eef54ba072b7b2be22d73bf80995f1
totostunt <tobias.brum@itisjt.edu.ar>
2024-05-06 21:45:48 -0300

    Agregar archivos de proyecto.

---
+++ 710MB/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Enum para el tipo de documento
+public enum Tipo_Documento
+{
+    DNI,
+    Pasaporte,
+    CarnetIdentidad
+}
+
+// Clase Puesto
+public class Puesto
+{
+    public string Nombre { get; set; }
+    
+}
